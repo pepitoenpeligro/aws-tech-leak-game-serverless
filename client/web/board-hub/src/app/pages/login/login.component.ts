d820665bf72a33d36628f8a29662f1353b4909ee
ToniPerea <apepa31@gmail.com>
2025-03-18 10:34:05 +0000

    feat: :zap: Add Click event to login page

--- client/web/board-hub/src/app/pages/login/login.component.ts
+++ client/web/board-hub/src/app/pages/login/login.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { InputBhComponent } from '../../../components/input-bh/input-bh.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonBhComponent } from '../../../components/button-bh/button-bh.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [InputBhComponent, MatCardModule, MatIcon],
+  imports: [InputBhComponent, MatCardModule, MatIcon, MatButtonModule, ButtonBhComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent { }
+export class LoginComponent {
+  onLoginClick() {
+    console.log('Login button clicked');
+  }
+}
