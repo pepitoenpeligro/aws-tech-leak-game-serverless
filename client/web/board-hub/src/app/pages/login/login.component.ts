4f092c18232581698b543a828824ac11bd3e64e0
ToniPerea <apepa31@gmail.com>
2025-03-18 11:16:18 +0000

    style: :art: Add Spinner to Login page

--- client/web/board-hub/src/app/pages/login/login.component.ts
+++ client/web/board-hub/src/app/pages/login/login.component.ts
@@ -4,15 +4,24 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonBhComponent } from '../../../components/button-bh/button-bh.component';
+import { SpinnerBhComponent } from '../../../components/spinner-bh/spinner-bh.component';
+import * as dadoAnimado from '../../../assets/lotties/dadoAnimado.json';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-login-page',
-  imports: [InputBhComponent, MatCardModule, MatIcon, MatButtonModule, ButtonBhComponent],
+  selector: 'login-page',
+  imports: [InputBhComponent, MatCardModule, MatIcon, MatButtonModule, ButtonBhComponent, SpinnerBhComponent, NgIf],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  dadoAnimado = dadoAnimado;
+  showSpinner = false;
+
   onLoginClick() {
-    console.log('Login button clicked');
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 10000);
   }
 }
